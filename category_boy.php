a8098d9c06192b39060b19eab6c0db24e17a2c82
dhsthdwjd <88641141+dhsthdwjd@users.noreply.github.com>
2022-09-04 14:56:19 +0900

    Update category_boy.php

--- category_boy.php
+++ category_boy.php
@@ -3,20 +3,20 @@
 	"name": "방탄소년단",
 	"eng" : "BTS",
 	"image": "https://raw.githubusercontent.com/dhsthdwjd/wallpaper_category/main/img_bts.webp"
-	"url": "worldwallpaper/aesthetic/allcat.php"
+	"url": "worldwallpaper/aesthetic/allcat_wallpaper.php"
 },
 
 {
 	"name": "세븐틴",
 	"eng" : "Seventeen",
 	"image": "https://raw.githubusercontent.com/dhsthdwjd/wallpaper_category/main/img_seventeen.webp"
-	"url": "worldwallpaper/aesthetic/flower.php"
+	"url": "worldwallpaper/aesthetic/flower_wallpaper.php"
 },
 
 {
 	"name": "엑소",
 	"eng" : "Exo",
 	"image": "https://raw.githubusercontent.com/dhsthdwjd/wallpaper_category/main/img_exo.webp"
-	"url": "worldwallpaper/aesthetic/food.php"
+	"url": "worldwallpaper/aesthetic/food_wallpaper.php"
 }
 ]
