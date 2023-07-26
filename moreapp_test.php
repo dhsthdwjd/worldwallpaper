7eaa1e7177462d5178ae7e3de6f663df1929b122
offsong <59704429+offsong@users.noreply.github.com>
2023-07-26 12:55:42 +0900

    Add files via upload

---
+++ moreapp_test.php
@@ -0,0 +1,8 @@
+[
+{
+	"eng": "복불복 룰렛 - 의사결정,제비뽑기,돌림판",
+	"name" : "Spin Wheel - Random Picker",
+	"rating": 3.0,
+	"image": "https://play-lh.googleusercontent.com/Ki6j8XkINeWcseUzxdzIAeskpI0O8ej7T2Y3KI2m_Ea3QDOhFSLGEdyyj_tujL5bIN4=w480-h960-rw"
+}
+]
